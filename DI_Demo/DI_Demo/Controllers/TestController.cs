5a9ee15ebe601c34da76239564d58468d4587d1a
kiettruong312 <30577490+kiettruong312@users.noreply.github.com>
2020-10-14 16:22:42 +0700

    Add files via upload

---
+++ DI_Demo/DI_Demo/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DI_Demo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DI_Demo.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private ISingletonHandler _singletonService;
+        private ITransientHandler _transientService;
+        private IScopedService _scopedService;
+        private TestService _testService;
+        public TestController(TestService testService, ISingletonHandler singletonService, ITransientHandler transientService, IScopedService scopedService)
+        {
+            _testService = testService;
+            _singletonService = singletonService;
+            _transientService = transientService;
+            _scopedService = scopedService;
+        }
+
+        [HttpGet]
+        public void Test()
+        {
+            _singletonService.Execute("");
+            _scopedService.Execute("");
+            _transientService.Execute("");
+            _testService.Execute();
+        }
+    }
+}
---
+++ DI_Demo/DI_Demo/Handlers/SingletonHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI_Demo.Services
+{
+    public class SingletonHandler : BaseHandler, ISingletonHandler
+    {
+        public void Execute(string hostService)
+        {
+            base.Log(hostService);
+        }
+    }
+}
